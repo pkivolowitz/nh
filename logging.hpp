f2fa90adfe5bbdb12538070ec137b85dd6e212e4
pkivolowitz <perry@kivolowitz.com>
2018-05-05 09:44:23 -0500

    Added a generic kind of logging line.

--- logging.hpp
+++ logging.hpp
@@ -6,4 +6,5 @@ extern std::ofstream log;
 #define	ENTERING()			if (log.is_open()) log << __FILE__ << " " << __FUNCTION__ << " " << __LINE__ << " entering" << endl
 #define	LEAVING()			if (log.is_open()) log << __FILE__ << " " << __FUNCTION__ << " " << __LINE__ << " leaving" << endl
 #define RETURNING(r)		if (log.is_open()) log << __FILE__ << " " << __FUNCTION__ << " " << __LINE__ << " returning " << (r) << endl
-	
+#define LOGMESSAGE(m)		if (log.is_open()) log << __FILE__ << " " << __FUNCTION__ << " " << __LINE__ << " " << m << endl
+
