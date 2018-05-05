6ca8d4fdf10febdf88521d237eac88239116f03b
pkivolowitz <perry@kivolowitz.com>
2018-05-04 22:12:51 -0500

    Added game clock, version command and neat TTY handling

--- game.hpp
+++ game.hpp
@@ -1,7 +1,9 @@
 #pragma once
 #include <vector>
+#include <unistd.h>
 #include "presentation.hpp"
 #include "level.hpp"
+#include "game_time.hpp"
 
 class Game {
 	public:
@@ -15,7 +17,14 @@ class Game {
 	private:
 		void AddLevel();
 		void EventLoop();
+		bool UpdateClock();
+		void Delay(useconds_t d = 32 * 1000);
+
 		bool HandleQuit();
+		void HandleVersion();
+
 		Presentation * p;
+		int current_level;
 		std::vector<Level *> levels;
+		GameTime gt;
 };
