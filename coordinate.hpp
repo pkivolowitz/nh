1a83428a6727a000d527af622c7bf5145864fe09
pkivolowitz <perry@kivolowitz.com>
2018-05-08 20:18:08 -0500

    added centroid

--- coordinate.hpp
+++ coordinate.hpp
@@ -2,10 +2,21 @@
 
 class Coordinate {
 	public:
-		Coordinate() { l = c = 0; }
-		Coordinate(int l, int c) { this->l = l; this->c = c; }
+		Coordinate() { 
+			l = c = 0; 
+		}
+		Coordinate(int l, int c) { 
+			this->l = l; this->c = c; 
+		}
 		int l, c;
 
-		Coordinate operator+(const Coordinate & rhs) { return Coordinate(this->l + rhs.l, this->c + rhs.c); }
+		Coordinate operator+(const Coordinate & rhs) { 
+			return Coordinate(this->l + rhs.l, this->c + rhs.c); 
+		}
+
+		static Coordinate Centroid(const Coordinate & a, const Coordinate & b) {
+			return Coordinate((a.l + b.l) / 2, (a.c + b.c) / 2);
+		}
+
 		void Clip(int L, int C) { if (l >= L - 1) l = L - 2; if (c >= C - 1) c = C - 2; }
 };
